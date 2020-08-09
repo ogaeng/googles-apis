193bab4ba1a625b9dc95085f2d4ef1aa09fc6f63
Ogaeng <ogaengent@gmail.com>
2020-08-10 03:14:25 +0900

    Create materialized.js

---
+++ materialized.js
@@ -0,0 +1,3 @@
+console.log('%c Copyright ⓒ 2020 %c ogaeng.com All rights reserved. ', 'background-color:#000000; color:#ffffff; font-size:23px;',
+'background-color:red; color:#ffffff; font-size:23px;');
+console.log('현재 Ogaeng의 강의가 아닌 다른 곳에서 이 사이트를 사용중이라면 무단으로 사용된 실습 페이지임을 알려드립니다.');
