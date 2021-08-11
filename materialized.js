9f762118596d8f67da8fd68dfa5f4f2bf23c8bc7
Ogaeng <ogaengent@gmail.com>
2021-08-11 13:06:38 +0900

    Update materialized.js

--- materialized.js
+++ materialized.js
@@ -1,3 +1,3 @@
-console.log('%c Copyright ⓒ 2020 %c ogaeng.com All rights reserved. ', 'background-color:#000000; color:#ffffff; font-size:23px;',
+console.log('%c Copyright ⓒ 2021 %c Opensource Marketing All rights reserved. ', 'background-color:#000000; color:#ffffff; font-size:23px;',
 'background-color:red; color:#ffffff; font-size:23px;');
-console.log('현재 Ogaeng의 강의가 아닌 다른 곳에서 이 사이트를 사용중이라면 무단으로 사용된 실습 페이지임을 알려드립니다.');
+console.log('오픈소스마케팅의 강의가 아닌 다른 강의에서 이 웹사이트를 사용중이라면 무단으로 사용된 실습 페이지임을 알려드립니다.');
