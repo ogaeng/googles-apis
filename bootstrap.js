63c7e9c39a8cd56c14ebd76a68c6d85e997f7514
Ogaeng <ogaengent@gmail.com>
2021-08-11 13:33:59 +0900

    nohze warning added

---
+++ bootstrap.js
@@ -0,0 +1,3 @@
+console.log('%c *** W A R N I N G *** %c Copyright ⓒ 2021 nohze.com All rights reserved. ', 'background-color:#000000; color:#ffffff; font-size:25px;',
+'background-color:yellow; color:#000000; font-size:20px;');
+console.log('현재 Nohze의 강의가 아닌 다른 곳에서 이 사이트를 사용중이라면 무단으로 사용된 실습 페이지임을 알려드립니다.');
